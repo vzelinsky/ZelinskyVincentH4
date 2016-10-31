4f40bb391837ec884668f0396d43c86b98a43234
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-30 22:20:50 -0400

    Initial commit for Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+/* Main.java(H4)
+ * Written by Vincent Zelinsky
+ * 
+ * Contains the methods to test Stack.java and Queue.java
+ */
+public class Main {
+  public static void main(String[] args) {
+    
+  }
+}
