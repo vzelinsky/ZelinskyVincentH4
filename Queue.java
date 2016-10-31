487c499829badf4d3b191c5e1959aed76c466abb
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-30 23:16:58 -0400

    Finishes methods of Queue.java

--- Queue.java
+++ Queue.java
@@ -11,8 +11,8 @@ public class Queue {
   private Node tail;
   
   //Constructors
-  //No argument constructor that when called creates an empty Stack
-  public Stack() {
+  //No argument constructor that when called creates an empty Queue
+  public Queue() {
     //Sets head to null
     this.head = null;
     //Sets tail to null
@@ -30,43 +30,51 @@ public class Queue {
     return tail;
   }
   
-  //push method
-  //Adds a Node to the top of the Stack as the new top
-  public void push(String dataInput) {
-    //Case 1: if the Stack is empty (head = null)
-    //Inserts the new Node at head
+  //enqueue method
+  //Adds a Node to the front of the Queue as the new end
+  public void enqueue(String dataInput) {
+    //Case 1: if the Queue is empty (head = null)
+    //Inserts the new Node at head and makes it the tail as it is the only Node
     if(this.head == null) {
       //Add the Node as the head
       this.head = new Node(dataInput);
+      //Set the tail to the same Node as head
+      this.tail = this.head;
     }
     else{
       Node n = new Node(dataInput);
-      n.setNext(head);
-      this.head = n;
+      tail.setNext(n);
+      this.tail = n;
     }
   }
-  //pop method
-  //Returns a Node from the top of the Stack, then removes it from the Stack
-  public Node pop() {
+  //dequeue method
+  //Returns a Node from the front of the Queue, then removes it from the Queue
+  public Node dequeue() {
     Node t = this.head;
-    this.head = head.getNext();
-    t.setNext(null);
+    if (this.head.getData().compareTo(this.tail.getData()) == 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    else {
+      this.head = head.getNext();
+      t.setNext(null);
+    }
     return(t);
   }
   //peek method
-  //Returns a Node from the top of the Stack
+  //Returns a Node from the top of the Queue
   public Node peek() {
     return this.head;
   }
   
-  //deleteStack method
-  //Removes references to the Stack, deleting the Stack
-  public void deleteStack() {
+  //deleteQueue method
+  //Removes references to the Queue, deleting the Queue
+  public void deleteQueue() {
     //Makes head null
     this.head = null;
     //Makes tail null
     this.tail = null;
     //Print success
-    System.out.println("The Stack has been deleted");
+    System.out.println("The Queue has been deleted");
   }
 }
