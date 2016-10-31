740e18494f51e3a2b4af2efafc6a131f7d91930c
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-30 22:58:47 -0400

    Adds tests to add method

--- Main.java
+++ Main.java
@@ -5,6 +5,17 @@
  */
 public class Main {
   public static void main(String[] args) {
+    Stack myStack = new Stack();
+    myStack.push("Alpha");
+    myStack.push("Bravo");
+    myStack.push("Delta");
+    myStack.push("Echo");
+    myStack.push("Charlie");
+    myStack.push("Foxtrot");
     
+    System.out.println(myStack.pop().getData());
+    System.out.println(myStack.pop().getData());
+    System.out.println(myStack.peek().getData());
+    System.out.println(myStack.pop().getData());
   }
 }
